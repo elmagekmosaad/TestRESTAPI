7e8d6b0bed668b0d6a34871909f1f6ded9c82fb1
elmagek mosaad <elmagekmosaad3535@gmail.com>
2024-02-27 09:00:44 +0200

    added Order Controller

---
+++ TestRESTAPI/Controllers/OrdersController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TestRESTAPI.Data;
+using TestRESTAPI.Data.Models;
+
+namespace TestRESTAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        private readonly AppDbContext _db;
+
+        public OrdersController(AppDbContext db)
+        {
+            _db = db;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAllOrders()
+        {
+            var orders = _db.Orders.ToArray();
+            return Ok(orders);
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddOrder(Order order)
+        {
+            return Ok(order);
+        }
+
+    }
+}
---
+++ TestRESTAPI/Data/Models/Order.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TestRESTAPI.Data.Models
+{
+    public class Order
+    {
+        [Key] 
+        public int Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public virtual ICollection<OrderItem>? OrderItems { get; set; }
+    }
+}
